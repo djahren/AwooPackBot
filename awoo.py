8cb6f1b3f9b7e008562084df9c7940c1865c675c
djahren <djahren@djahren.com>
2022-08-12 11:46:22 -0700

    add *action*_daily_reminder commands and refactor

--- awoo.py
+++ awoo.py
@@ -1,71 +1,137 @@
-import logging, re, sys, json
-from random import choice
-from zoneinfo import ZoneInfo
+from glob import glob
+import logging, re, sys
+from constants import *
+from functions import *
 from telegram import Update,Chat
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
-from datetime import time,timedelta,datetime
-import pandas as pd
+from datetime import datetime,time,timedelta
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-sheet_id = "1IfGrcY4ntE70fycFRAEtjAvb20ukVf9wTkPzdvtLLKg"
-formats_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Formats"
-words_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Words"
-pacific_tz = ZoneInfo("America/Los_Angeles")
-chats_file_path = "data/chats.json"
-formats = ()
-words = {}
+data = {}
 chats = {}
+msg = get_system_messages()
 
-def update_data_from_google():
-    global formats
-    formats_pd = pd.read_csv(formats_url).to_dict()
-    words_pd = pd.read_csv(words_url).to_dict()
+def register_daily_reminder(context: ContextTypes.DEFAULT_TYPE, chat_id, hours, minutes = 0):
+    first_occurance = time(hour=hours, minute=minutes, tzinfo=PACIFIC_TZ)
+    job_name = get_job_name(chat_id, hours, minutes)
+    return context.job_queue.run_repeating(send_reminder_job, interval=timedelta(days=1), first=first_occurance, chat_id=chat_id, name=job_name)
+    
 
-    new_formats = []
-    for key in formats_pd["format"]:
-        new_formats.append(formats_pd["format"][key])
-    formats = tuple(new_formats)
+def add_chat_if_not_exist(chat: Chat):
+    #add the chat id to the current chats list if it doesn't exist 
+    #append the file if the chat id doesn't currently exist in the file with the format <chatid>,<chatname>
+    if not chat.id in chats:
+        title = chat.title if chat.title else f"{chat.first_name} {chat.last_name}"
+        chats[chat.id] = {
+            "title": title,
+            "daily_reminders": [],
+            "onetime_reminders": [],
+            "time_zone": "America/Los_Angeles"
+        }
+        save_chats_to_file(chats)
 
-    for key in [w for w in words_pd if w.find("Unnamed") == -1]:
-        new_words = []
-        for word_key in words_pd[key]:
-            word = words_pd[key][word_key]
-            if isinstance(word, str):
-                new_words.append(word)
-        words[key] = new_words
+def remove_chat(chat_id):
+    chats.pop(chat_id)
+    save_chats_to_file(chats)
 
-def generate_message():
-    the_message = str(choice(formats)) #pick a format
-    now = datetime.now(pacific_tz)
-    if now.hour < 12:   tod = "morning" 
-    elif now.hour < 18: tod = "afternoon"
-    else:               tod = "evening"
+def load_chats(application):
+    global chats
+    chats = get_chats_from_file()
+    for chat_id in chats: #load reminders
+        logging.info(chat_id)
+        context = ContextTypes.DEFAULT_TYPE(application=application, chat_id=chat_id)
+        for daily_id in chats[chat_id]["daily_reminders"]:
+            register_daily_reminder(context=context, chat_id=chat_id, hours=int(daily_id.split("_")[1]), minutes=int(daily_id.split("_")[2]))
 
-    vars_to_replace = re.findall(r'\%[a-z_]+\%',the_message) #get all variables
-    for index, current_var in enumerate(vars_to_replace): #loop through and replace each one
-        key = str(current_var).replace('%','')
-        if key == 'reminder': #set the reminder to the first reminder if it's morning, else pick at random
-            selected_word = words[key][0] if tod == 'morning' else str(choice(words[key][1:])).strip()
-        else:
-            selected_word = str(choice(words[key])).strip() #select random word for each variable
-        if key == 'greeting' and index != 0: 
-            selected_word = selected_word.lower()
-        the_message = the_message.replace(current_var, selected_word, 1)
+async def send_reminder_job(context: ContextTypes.DEFAULT_TYPE) -> None: #called by scheduled job
+    job = context.job
+    message = generate_message(data)
+    logging.info(f"Sending message via job: {message} at {get_current_time_string()}")
+    await context.bot.send_message(chat_id=job.chat_id, text=message)  
 
-    return the_message.replace('%tod%', tod) #return message and replace %tod% if it exists
+async def send_message_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = generate_message(data)
+    logging.info(f"Sending message: {message} at {get_current_time_string()}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
-def register_jobs(context: ContextTypes.DEFAULT_TYPE, chat_id):
-    m = context.job_queue.run_repeating(send_reminder_job, interval=timedelta(days=1), first=time(hour=8, tzinfo=pacific_tz),chat_id=chat_id, name=f"morning{chat_id}")
-    a = context.job_queue.run_repeating(send_reminder_job, interval=timedelta(days=1), first=time(hour=13, tzinfo=pacific_tz),chat_id=chat_id, name=f"afternoon{chat_id}")
-    return m and a
+async def list_daily_reminders_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    if chat_id in chats:
+        reminders = chats[chat_id]["daily_reminders"]
+        if reminders:
+            reminders_list_msg = "This chat has the following reminders set:\n"
+            for re_key in reminders: 
+                hours = int(re_key.split("_")[1])
+                minutes = int(re_key.split("_")[2])
+                reminders_list_msg += f"{hours:02d}:{minutes:02d}\n"
+            await context.bot.send_message(chat_id=chat_id, text=reminders_list_msg)       
+            return
+    await context.bot.send_message(chat_id=chat_id, text=msg["err_no_reminders"])        
 
-def unregister_jobs(context: ContextTypes.DEFAULT_TYPE, chat_id):
+async def set_daily_reminder_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    if context.args:
+        matches = match_time_string(time_string=context.args[0])
+        if matches:
+            hours,minutes = matches
+            add_chat_if_not_exist(update.effective_chat)
+            job_name = get_job_name(chat_id, hours, minutes)
+            if not job_name in chats[chat_id]["daily_reminders"]:
+                job = register_daily_reminder(context=context, chat_id=chat_id, hours=hours, minutes=minutes)
+                if job: 
+                    chats[chat_id]["daily_reminders"].append(job_name)
+                    save_chats_to_file(chats)
+                    await context.bot.send_message(chat_id=chat_id, text=msg["cmd_set_daily_succcess"])
+                else:
+                    await context.bot.send_message(chat_id=chat_id, text=msg["err_cant_schedule_jobs"])
+            else:
+                await context.bot.send_message(chat_id=chat_id, text=msg["err_already_exists"])
+            return  
+    await context.bot.send_message(chat_id=chat_id, text=msg["err_24h_format"])
+
+async def stop_daily_reminder_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    if context.args:
+        matches = match_time_string(time_string=context.args[0])
+        if matches:
+            hours,minutes = matches
+            job_name = get_job_name(chat_id, hours=hours, minutes=minutes)
+            current_jobs = context.job_queue.get_jobs_by_name(job_name)
+            if current_jobs:
+                job = current_jobs[0]
+                job.schedule_removal()
+                logging.info(f"Removing job: {job_name}")
+                await context.bot.send_message(chat_id=chat_id, text=msg["cmd_stop_daily_success"])
+                if job_name in chats[chat_id]["daily_reminders"]:
+                    chats[chat_id]["daily_reminders"].remove(job_name)
+                    save_chats_to_file(chats)
+            else:
+                await context.bot.send_message(chat_id=chat_id, text=msg["err_cant_find_reminder"])
+            return
+    await context.bot.send_message(chat_id=chat_id, text=msg["err_24h_format"])
+
+async def update_data_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global data
+    data = get_data_from_google()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=msg["cmd_update"])
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=msg["cmd_help"])
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_message.chat_id
+    add_chat_if_not_exist(update.effective_message.chat)
+    await context.bot.send_message(chat_id=chat_id, text=msg["cmd_start"])
+
+async def stop_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_message.chat_id
     removed_jobs = False
-    for job_name in (f"morning{chat_id}",f"afternoon{chat_id}"):
+    for job_name in (get_job_name(chat_id, 8, 0),get_job_name(chat_id, 13, 0)):
         current_jobs = context.job_queue.get_jobs_by_name(job_name)
         for job in current_jobs:
             job.schedule_removal()
@@ -73,89 +139,14 @@ def unregister_jobs(context: ContextTypes.DEFAULT_TYPE, chat_id):
             removed_jobs = True
     if removed_jobs:
         remove_chat(chat_id=chat_id)
-    return removed_jobs
-
-def save_chat(chat: Chat):
-    #add the chat id to the current chats list if it doesn't exist 
-    #append the file if the chat id doesn't currently exist in the file with the format <chatid>,<chatname>
-    if not chat.id in chats:
-        title = chat.title if chat.title else f"{chat.first_name} {chat.last_name}"
-        chats[chat.id] = {"title": title}
-        save_file()
-
-def save_file():
-    with open(chats_file_path, "w") as chat_file:
-        chat_file.write(json.dumps(chats, indent=4))
-
-def load_file():
-    global chats
-    try:
-        with open(chats_file_path, "r") as chat_file:
-            chats = json.load(chat_file)
-    except FileNotFoundError:
-        chats = {}
-    new_chats = {}
-    for chat_id in chats: #convert all chat_id dictionary keys saved as strings to numbers.
-        if isinstance(chat_id, str) and chat_id.lstrip('-').isnumeric():
-            new_chats[int(chat_id)] = chats[chat_id]
-        else:
-            new_chats[chat_id] = chats[chat_id]
-    chats = new_chats.copy()
-    print(chats)
-
-def remove_chat(chat_id):
-    chats.pop(chat_id)
-    save_file()
-
-def load_chats(application):
-    load_file()
-    for chat_id in chats:
-        logging.info(chat_id)
-        context = ContextTypes.DEFAULT_TYPE(application=application, chat_id=chat_id)
-        register_jobs(context=context, chat_id=chat_id)
-
-def get_current_time_string():
-    return datetime.now(pacific_tz).strftime("%H:%M:%S")
-
-async def send_reminder_job(context: ContextTypes.DEFAULT_TYPE) -> None: #called by scheduled job
-    job = context.job
-    message = generate_message()
-    logging.info(f"Sending message via job: {message} at {get_current_time_string()}")
-    await context.bot.send_message(chat_id=job.chat_id, text=message)  
-
-async def send_message_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message = generate_message()
-    logging.info(f"Sending message: {message} at {get_current_time_string()}")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-async def update_data_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    update_data_from_google()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I've updated the my database from the Google Sheet.")
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Available commands:\n/getmessage: gets a random message.\n/help: shows this message.\n/start: registers recurring messages.\n/stop: removes scheduled reminders for this chat.\n/update: updates the bot's data from the database.")
-
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_message.chat_id
-    jobs_scheduled = register_jobs(context=context, chat_id=chat_id)
-    if jobs_scheduled: 
-        save_chat(update.effective_message.chat)
-        await context.bot.send_message(chat_id=chat_id, text="Awo0o0o! I've registered morning and afternoon repeating reminders for you.\nUse /help to see a list of commands I respond to.")
-    else:
-        await context.bot.send_message(chat_id=chat_id, text="I had trouble scheduling the default jobs. 🥺 I'm sorry, please check the logs for more info.")
-
-async def stop_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_message.chat_id
-    jobs_stopped = unregister_jobs(context=context, chat_id=chat_id)
-    if jobs_stopped: await context.bot.send_message(chat_id=chat_id, text="I've removed the scheduled reminders from this chat.")
-    else: await context.bot.send_message(chat_id=chat_id, text="I'm not seeing any scheduled reminders for this chat.")
+        await context.bot.send_message(chat_id=chat_id, text=msg["cmd_stop"])
+    else: await context.bot.send_message(chat_id=chat_id, text=msg["err_no_reminders"])
 
 async def unknown_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=msg["cmd_unknown"])
 
 if __name__ == '__main__':
-    update_data_from_google()
-    
+    data = get_data_from_google()
     #open token.txt
     token = ""
     with open("token.txt", "r") as tkfile:
@@ -165,10 +156,13 @@ if __name__ == '__main__':
     
     application = ApplicationBuilder().token(token).build()
     load_chats(application)
+    application.add_handler(CommandHandler('help', help_command))
+    application.add_handler(CommandHandler('getmessage', send_message_command))
+    application.add_handler(CommandHandler('listdailyreminders', list_daily_reminders_command))
+    application.add_handler(CommandHandler('setdailyreminder', set_daily_reminder_command))
+    application.add_handler(CommandHandler('stopdailyreminder', stop_daily_reminder_command))
     application.add_handler(CommandHandler('start', start_command))
     application.add_handler(CommandHandler('stop', stop_command))
-    application.add_handler(CommandHandler('getmessage', send_message_command))
-    application.add_handler(CommandHandler('help', help_command))
     application.add_handler(CommandHandler('update', update_data_command))
     application.add_handler(MessageHandler(filters.COMMAND, unknown_command))
 
---
+++ functions.py
@@ -0,0 +1,83 @@
+#helper funtions
+from constants import *
+import json,re
+from random import choice
+from datetime import datetime
+import pandas as pd
+
+def get_data_from_google():
+    formats_url = f"https://docs.google.com/spreadsheets/d/{GOOGLE_SHEET_ID}/gviz/tq?tqx=out:csv&sheet=Formats"
+    words_url = f"https://docs.google.com/spreadsheets/d/{GOOGLE_SHEET_ID}/gviz/tq?tqx=out:csv&sheet=Words"
+    formats_pd = pd.read_csv(formats_url).to_dict()
+    words_pd = pd.read_csv(words_url).to_dict()
+    data = {}
+    new_formats = []
+    for key in formats_pd["format"]:
+        new_formats.append(formats_pd["format"][key])
+    data["formats"] = tuple(new_formats)
+
+    data["words"] = {}
+    for key in [w for w in words_pd if w.find("Unnamed") == -1]:
+        new_words = []
+        for word_key in words_pd[key]:
+            word = words_pd[key][word_key]
+            if isinstance(word, str):
+                new_words.append(word)
+        data["words"][key] = new_words
+    return data
+
+def generate_message(data):
+    words = data["words"]
+    formats = data["formats"]
+    the_message = str(choice(formats)) #pick a format
+    now = datetime.now(PACIFIC_TZ)
+    if now.hour < 12:   tod = "morning" 
+    elif now.hour < 18: tod = "afternoon"
+    else:               tod = "evening"
+
+    vars_to_replace = re.findall(r'\%[a-z_]+\%',the_message) #get all variables
+    for index, current_var in enumerate(vars_to_replace): #loop through and replace each one
+        key = str(current_var).replace('%','')
+        if key == 'reminder': #set the reminder to the first reminder if it's morning, else pick at random
+            selected_word = words[key][0] if tod == 'morning' else str(choice(words[key][1:])).strip()
+        else:
+            selected_word = str(choice(words[key])).strip() #select random word for each variable
+        if key == 'greeting' and index != 0: 
+            selected_word = selected_word.lower()
+        the_message = the_message.replace(current_var, selected_word, 1)
+
+    return the_message.replace('%tod%', tod) #return message and replace %tod% if it exists
+
+def get_job_name(chat_id, hours, minutes):
+    return f"{chat_id}_{hours}_{minutes}"
+
+def get_current_time_string():
+    return datetime.now(PACIFIC_TZ).strftime("%H:%M:%S")
+
+def match_time_string(time_string) -> dict:
+    matches = re.match(r'([0-2]?\d):??([0-5]\d)', time_string)
+    if matches: return (int(matches[1]), int(matches[2]))
+    else: return {}
+
+def get_system_messages():
+    with open(MESSAGES_FILE_PATH, "r") as msg_file:
+        return json.load(msg_file)
+
+def save_chats_to_file(chats):
+    with open(CHATS_FILE_PATH, "w") as chat_file:
+        chat_file.write(json.dumps(chats, indent=4))
+
+def get_chats_from_file():
+    try:
+        with open(CHATS_FILE_PATH, "r") as chat_file:
+            chats = json.load(chat_file)
+    except:
+        chats = {}
+    
+    new_chats = {}
+    for chat_id in chats: #convert all chat_id dictionary keys saved as strings to numbers.
+        if isinstance(chat_id, str) and chat_id.lstrip('-').isnumeric():
+            new_chats[int(chat_id)] = chats[chat_id]
+        else:
+            new_chats[chat_id] = chats[chat_id]
+    return new_chats
